e374a70d6ac8a7a347a0e789ae97e7e50e6c12cd
Maskode <maskodestudios@gmail.com>
2017-04-17 18:46:23 -0300

    update Lexa
    añadido scene de intro, y una scene vacia que posteriormente sera el
    menu

---
+++ MelodyRUSH v1.0.0/Assets/Intro/MovieIntroScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class MovieIntroScript : MonoBehaviour {
+
+    public string sceneToLoad = "Menu";
+    public void LoadMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(sceneToLoad);
+    }
+}
